051ab0260ab1f171c333f109232382d82bd391ed
Mihai Leonte <mihai.leonte@me.com>
2017-12-21 21:57:53 +0200

    changed required info - separate init calls

--- AccessPassGenerator/Person.swift
+++ AccessPassGenerator/Person.swift
@@ -18,6 +18,22 @@ enum Entrant {
     case manager
 }
 
+enum Guest {
+    case classic
+    case vip
+    case freeChild
+}
+
+enum HourlyEmployee {
+    case foodServices
+    case rideServices
+    case maintenance
+}
+
+enum Manager {
+    case manager
+}
+
 enum AreaAccess {
     case amusement
     case kitchen
@@ -37,36 +53,72 @@ enum Discount {
     case merchandise(percentage: Int)
 }
 
-enum RequiredInfo {
-    case birthDate
-    case firstName
-    case lastName
-    case streetAddress
-    case city
-    case state
-    case zipCode
-    case SSN
-    case managementTier
+struct RequiredInformation {
+    var birthDate: Date?
+    var firstName: String?
+    var lastName: String?
+    var streetName: String?
+    var city: String?
+    var state: String?
+    var zipCode: String?
+    var SSN: String?
 }
 
 protocol PersonType {
-    var entrant: Entrant { get }
     var areaAccess: [AreaAccess] { get }
     var rideAccess: [RideAccess] { get }
     var discountAccess: [Discount] { get }
-    var requiredInformation: [RequiredInfo]? { get }
+    var requiredInformation: RequiredInformation? { get }
     
-    init(ofType type: Entrant)
+    //init(ofType type: Entrant)
+    init(ofType type: Guest, birthDate: Date?)
+    init(ofType type: HourlyEmployee, firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String, SSN: String, birthDate: Date?)
+    func swipeArea(in area: AreaAccess) -> Bool
+    func swipeRide(in ride: RideAccess) -> Bool
 }
 
 class Person: PersonType {
-    var entrant: Entrant
     var areaAccess: [AreaAccess]
     var rideAccess: [RideAccess]
     var discountAccess: [Discount]
-    var requiredInformation: [RequiredInfo]?
+    var requiredInformation: RequiredInformation?
     
     
+    required init(ofType type: Guest, birthDate: Date?) {
+        switch type {
+        case .classic:
+            self.areaAccess = [.amusement]
+            self.rideAccess = [.all]
+            self.discountAccess = [.none]
+        case .vip:
+            self.areaAccess = [.amusement]
+            self.rideAccess = [.all, .skipLines]
+            self.discountAccess = [.food(percentage: 10), .merchandise(percentage: 20)]
+        case .freeChild:
+            self.areaAccess = [.amusement]
+            self.rideAccess = [.all]
+            self.discountAccess = [.none]
+            requiredInformation = RequiredInformation(birthDate: birthDate)
+        }
+    }
+    
+    required init(ofType type: HourlyEmployee, firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String, SSN: String, birthDate: Date?) {
+        switch type {
+        case .foodServices:
+            self.areaAccess = [.amusement, .kitchen]
+            self.rideAccess = [.all]
+            self.discountAccess = [.food(percentage: 15), .merchandise(percentage: 25)]
+        case .rideServices:
+            self.areaAccess = [.amusement, .rideControl]
+            self.rideAccess = [.all]
+            self.discountAccess = [.food(percentage: 15), .merchandise(percentage: 25)]
+        case .maintenance:
+            self.areaAccess = [.amusement, .kitchen, .rideControl, .maintenance]
+            self.rideAccess = [.all]
+            self.discountAccess = [.food(percentage: 15), .merchandise(percentage: 25)]
+        }
+    }
+    /*
     required init(ofType type: Entrant) {
         self.entrant = type
         
@@ -105,6 +157,27 @@ class Person: PersonType {
             self.discountAccess = [.food(percentage: 25), .merchandise(percentage: 25)]
             self.requiredInformation = [.firstName, .lastName, .streetAddress, .city, .state, .zipCode, .SSN, .birthDate, .managementTier]
         }
+        
+        
+    }
+ */
+    
+    func swipeArea(in area: AreaAccess) -> Bool {
+        for areaWithAccess in self.areaAccess {
+            if areaWithAccess == area {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func swipeRide(in ride: RideAccess) -> Bool {
+        for rideWithAccess in self.rideAccess {
+            if rideWithAccess == ride {
+                return true
+            }
+        }
+        return false
     }
 }
 
--- AccessPassGenerator/ViewController.swift
+++ AccessPassGenerator/ViewController.swift
@@ -12,14 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+       // let guest1 = Person(ofType: .classicGuest)
+       // print(guest1.swipeArea(in: .kitchen))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
 
